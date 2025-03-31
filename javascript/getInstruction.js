a0160991c940a67cfa7693aeafe1589d31b5c685
marcardus <marcardus@gmail.com>
2025-03-31 10:37:12 +0200

    first commit

--- javascript/getInstruction.js
+++ javascript/getInstruction.js
@@ -2,6 +2,17 @@
 /****** DO NOT TOUCH vvv *****/
 
 // Callback based function
+
+function addFood(step, id, callback) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('step ', step, 'id ', id);
+      document.querySelector(id).innerHTML += `<li>${step}</li`;
+      resolve(step);
+      callback ? callback() : ''; // For iteration one
+    }, Math.floor(Math.random() * 1000));
+  });
+}
 function getInstruction(food, step, callback, errorCallback) {
     setTimeout(() => {
       // Get the instruction string
--- javascript/index.js
+++ javascript/index.js
@@ -28,13 +28,68 @@
 
 
 // Iteration 1 - using callbacks
-// ...
+const cookSteak = new Promise((resolve, reject) => {
+  addFood(steak[0], "#steak", () => {
+    addFood(steak[1], "#steak", () => {
+      addFood(steak[2], "#steak", () => {
+        addFood(steak[3], "#steak", () => {
+          addFood(steak[4], "#steak", () => {
+            addFood(steak[5], "#steak", () => {
+              addFood(steak[6], "#steak", () => {
+                addFood(steak[7], "#steak", () => {
+                  document.querySelector(
+                    "#table"
+                  ).innerHTML += `<img src="public/images/steak.jpg" />`;
+                  resolve();
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  });
+});
 
 // Iteration 2 - using promises
-// ...
+const cookMash = new Promise((resolve, reject) => {
+  addFood(mashPotatoes[0], "#mashPotatoes").then(() => {
+    addFood(mashPotatoes[1], "#mashPotatoes").then(() => {
+      addFood(mashPotatoes[2], "#mashPotatoes").then(() => {
+        addFood(mashPotatoes[3], "#mashPotatoes").then(() => {
+          addFood(mashPotatoes[4], "#mashPotatoes").then(() => {
+            document.querySelector(
+              "#table"
+            ).innerHTML += `<img src="public/images/mashPotatoes.jpg" />`;
+
+            resolve();
+          });
+        });
+      });
+    });
+  });
+});
+
 
 // Iteration 3 using async/await
-// ...
+async function makeFood(steps, id) {
+  return new Promise(async (resolve, reject) => {
+    for (let step of steps) {
+      await addFood(step, id);
+    }
+
+    document.querySelector(
+      "#table"
+    ).innerHTML += `<img src="public/images/brusselSprouts.jpg" />`;
+
+    resolve();
+  });
+}
+
+const cookBrusselSprouts = makeFood(brusselsSprouts, "#brusselSprouts");
 
 // Bonus 2 - Promise all
-// ...
+const audio = new Audio("public/media/dinnerIsServed.mp3");
+audio.volume = 0.5;
+audio.play();
+alert("Dinner is served!");
